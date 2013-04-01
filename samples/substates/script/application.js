e2c630cbfe4936231388caee7871d6bcfdc7f060
jmelgaard <admin@it-links.dk>
2013-04-01 10:21:43 +0200

    more on sample

--- samples/substates/script/application.js
+++ samples/substates/script/application.js
@@ -1,117 +1,147 @@
 ﻿var app = angular.module('sample', ['ui.routing']);app.config(['$stateProvider', '$routeProvider',       function ($stateProvider, $routeProvider) {
-           $routeProvider               .otherwise({ redirectTo: '/' });
-           $stateProvider               .state('home', {
-                   route: '/',                   views: {
-                       'main': {
-                           template: 'tpl/home.html',                           controller: function ($rootScope) { $rootScope.page = "home"; }
-                       },                       'crumbs': {
-                           template: 'tpl/crumbs.html',
-                           controller: function ($scope) {
-                               $scope.crumbs = [
-                                   { link: '#/', title:'home' }
-                               ];
-                           }
-                       }
-                   }
-               })               .state('blog', {
-                   route: '/blog',                   views: {
-                       'main': {
-                           template: 'tpl/blog.html',                           controller: function ($rootScope, $scope, blog) {
-                               $rootScope.page = "blog";                               $scope.categories = blog.getCategories();                               $scope.archives = blog.getArchives();
-                           }
-                       },                       'crumbs': {
-                           template: 'tpl/crumbs.html',
-                           controller: function ($scope) {
-                               $scope.crumbs = [
-                                   { link: '#/blog', title:'blog' }
-                               ];
-                           }
-                       },                       'content': {
-                           template: 'tpl/blog.list.html',                           controller: function ($scope, blog) {
-                               $scope.title = "Recent Posts";
-                               $scope.posts = blog.getRecentPosts();
-                           }
-                       }
-                   }
-               })               .state('blog.category', {
-                   route: '/category/{category}',                   views: {
-                       'crumbs': {
-                           template: 'tpl/crumbs.html',
-                           controller: function ($scope, $routeParams) {
-                               $scope.crumbs = [
-                                   { link: '#/blog', title: 'blog' },
-                                   { link: '#/blog/category/' + $routeParams.category, title: "Category: " + $routeParams.category }
-                               ];
-                           }
-                       },                       'content': {
-                           template: 'tpl/blog.list.html',                           controller: function ($scope, $routeParams, blog) {
-                               $scope.title = $routeParams.category;
-                               $scope.posts = blog.getPostsByCategory($routeParams.category);
-                           }
-                       }
-                   }
-               })               .state('blog.archive', {
-                   route: '/archive/{archive}',                   views: {
-                       'crumbs': {
-                           template: 'tpl/crumbs.html',
-                           controller: function ($scope, $routeParams) {
-                               $scope.crumbs = [
-                                   { link: '#/blog', title: 'blog' },
-                                   { link: '#/blog/archive/' + $routeParams.archive, title: "Archive: "+$routeParams.archive }
-                               ];
-                           }
-                       },
-                       'content': {
-                           template: 'tpl/blog.list.html',                           controller: function ($scope, $routeParams, blog) {
-                               $scope.title = $routeParams.archive;
-                               $scope.posts = blog.getPostsByArchive($routeParams.archive);
-                           }
-                       }
-                   }
-               })               .state('blog.post', {
-                   route: '/post/{post}',                   views: {
-                       'crumbs': {
-                           template: 'tpl/crumbs.html',
-                           controller: function ($scope, $routeParams) {
-                               $scope.crumbs = [
-                                   { link: '#/blog', title: 'blog' },
-                                   { link: '#/blog/post/' + $routeParams.post, title: "Post: " + $routeParams.post }
-                               ];
-                           }
-                       },                       'content': {
-                           template: 'tpl/blog.post.html',                           controller: function ($scope, $routeParams, blog) {
-                               printStack("Running controller for content");
-                               var post = blog.getPost($routeParams.post);
-                               $scope.post = post;
-                           }
-                       }
-                   }
-               })               .state('blog.post.comments', {
-                   route: '/comments',                   views: {
-                       'crumbs': {
-                           template: 'tpl/crumbs.html',
-                           controller: function ($scope, $routeParams) {
-                               $scope.crumbs = [
-                                   { link: '#/blog', title: 'blog' },
-                                   { link: '#/blog/post/' + $routeParams.post, title: $routeParams.post }
-                               ];
-                           }
-                       },
-                   }
-               })               .state('about', {
-                   route: '/about',                   views: {
-                       'main': {
-                           template: 'tpl/about.html',                           controller: function ($rootScope) { $rootScope.page = "about"; }
-                       },                       'crumbs': {
-                           template: 'tpl/crumbs.html',
-                           controller: function ($scope) {
-                               $scope.crumbs = [
-                                   { link: '#/about', title: 'about' }
-                               ];
-                           }
-                       },
-                   }
-               })               .transition('*', '*', function ($rootScope) { $rootScope.transition = "global handler"; })               .transition('*', 'home', function ($rootScope) { $rootScope.transition = "root -> home"; })               .transition('*', 'blog', function ($rootScope) { $rootScope.transition = "root -> blog"; })               .transition('*', 'about', function ($rootScope) { $rootScope.transition = "root -> about"; })               .transition('home', 'blog', function ($rootScope) { $rootScope.transition = "home -> blog"; })               .transition('home', 'about', function ($rootScope) { $rootScope.transition = "home -> about"; })               .transition('blog', 'home', function ($rootScope) { $rootScope.transition = "blog -> home"; })               .transition('blog', 'about', function ($rootScope) { $rootScope.transition = "blog -> about"; })               .transition('about', 'home', function ($rootScope) { $rootScope.transition = "about -> home"; })               .transition('about', 'blog', function ($rootScope) { $rootScope.transition = "about -> blog"; });
+           $routeProvider               .otherwise({ redirectTo: '/' });
+
+            $stateProvider
+                .state('home', {
+                    route: '/',
+                    views: {
+                        'main': {
+                            template: 'tpl/home.html',
+                            controller: function($rootScope) { $rootScope.page = "home"; }
+                        },
+                        'crumbs': {
+                            template: 'tpl/crumbs.html',
+                            controller: function($scope) {
+                                $scope.crumbs = [
+                                    { link: '#/', title: 'home' }
+                                ];
+                            }
+                        }
+                    }
+                })
+                .state('blog', {
+                    route: '/blog',
+                    views: {
+                        'main': {
+                            template: 'tpl/blog.html',
+                            controller: function($rootScope, $scope, blog) {
+                                $rootScope.page = "blog";
+                                $scope.categories = blog.getCategories();
+                                $scope.archives = blog.getArchives();
+                            }
+                        },
+                        'crumbs': {
+                            template: 'tpl/crumbs.html',
+                            controller: function($scope) {
+                                $scope.crumbs = [
+                                    { link: '#/blog', title: 'blog' }
+                                ];
+                            }
+                        },
+                        'content': {
+                            template: 'tpl/blog.list.html',
+                            controller: function($scope, blog) {
+                                $scope.title = "Recent Posts";
+                                $scope.posts = blog.getRecentPosts();
+                            }
+                        }
+                    }
+                })
+                .state('blog.category', {
+                    route: '/category/{category}',
+                    views: {
+                        'crumbs': {
+                            template: 'tpl/crumbs.html',
+                            controller: function($scope, $routeParams) {
+                                $scope.crumbs = [
+                                    { link: '#/blog', title: 'blog' },
+                                    { link: '#/blog/category/' + $routeParams.category, title: "Category: " + $routeParams.category }
+                                ];
+                            }
+                        },
+                        'content': {
+                            template: 'tpl/blog.list.html',
+                            controller: function($scope, $routeParams, blog) {
+                                $scope.title = $routeParams.category;
+                                $scope.posts = blog.getPostsByCategory($routeParams.category);
+                            }
+                        }
+                    }
+                })
+                .state('blog.archive', {
+                    route: '/archive/{archive}',
+                    views: {
+                        'crumbs': {
+                            template: 'tpl/crumbs.html',
+                            controller: function($scope, $routeParams) {
+                                $scope.crumbs = [
+                                    { link: '#/blog', title: 'blog' },
+                                    { link: '#/blog/archive/' + $routeParams.archive, title: "Archive: " + $routeParams.archive }
+                                ];
+                            }
+                        },
+                        'content': {
+                            template: 'tpl/blog.list.html',
+                            controller: function($scope, $routeParams, blog) {
+                                $scope.title = $routeParams.archive;
+                                $scope.posts = blog.getPostsByArchive($routeParams.archive);
+                            }
+                        }
+                    }
+                })
+                .state('blog.post', {
+                    route: '/post/{post}',
+                    views: {
+                        'crumbs': {
+                            template: 'tpl/crumbs.html',
+                            controller: function($scope, $routeParams) {
+                                $scope.crumbs = [
+                                    { link: '#/blog', title: 'blog' },
+                                    { link: '#/blog/post/' + $routeParams.post, title: "Post: " + $routeParams.post }
+                                ];
+                            }
+                        },
+                        'content': {
+                            template: 'tpl/blog.post.html',
+                            controller: function($scope, $routeParams, blog) {
+                                printStack("Running controller for content");
+                                var post = blog.getPost($routeParams.post);
+                                $scope.post = post;
+                            }
+                        }
+                    }
+                })
+                .state('blog.post.comments', {
+                    route: '/comments',
+                    views: {
+                        'crumbs': {
+                            template: 'tpl/crumbs.html',
+                            controller: function($scope, $routeParams) {
+                                $scope.crumbs = [
+                                    { link: '#/blog', title: 'blog' },
+                                    { link: '#/blog/post/' + $routeParams.post, title: $routeParams.post }
+                                ];
+                            }
+                        },
+                    }
+                })
+                .state('about', {
+                    route: '/about',
+                    views: {
+                        'main': {
+                            template: 'tpl/about.html',
+                            controller: function($rootScope) { $rootScope.page = "about"; }
+                        },
+                        'crumbs': {
+                            template: 'tpl/crumbs.html',
+                            controller: function($scope) {
+                                $scope.crumbs = [
+                                    { link: '#/about', title: 'about' }
+                                ];
+                            }
+                        },
+                    }
+                });
        }]);
 function clean(state) {
     var newState = {};
