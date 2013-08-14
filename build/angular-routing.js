9203fa0f10dbfde7f6bf65ac8da8039b36c91d63
Jens Melgaard <admin@it-links.dk>
2013-08-14 13:55:30 +0200

    basic documentation of $state and $stateProvider, close #26, close #27

--- build/angular-routing.js
+++ build/angular-routing.js
@@ -1198,7 +1198,118 @@ angular.module('dotjem.routing').provider('$stateTransition', $StateTransitionPr
 * @name dotjem.routing.$stateProvider
 *
 * @description
-* Used for configuring states. See {@link dotjem.routing.$state $state} for an example.
+* Used for configuring states.
+* <br/>
+* Here is a very basic example of configuring states.
+*
+* <pre>
+* angular.module('demo', ['dotjem.routing']).
+*   config(['$stateProvider', function($stateProvider) {
+*   $stateProvider
+*       .state('phones', { /*.. Parameters for the state ..*\/ })
+*       .state('tablets', { /*.. Parameters for the state ..*\/ });
+* }]);
+* </pre>
+*
+* In it self that is not really useful, but the state it self can have views added as well as onEnter / onExit handlers.
+* <br/>
+* ### Views
+* <hr/>
+* At this basic level you can also configure multiple views, just add a number of `ui - view` directives with unique names, and simply target those from the configuration.
+* <br/>
+* e.g.if we had a `main` view and a `hint` view we could do.
+*
+* <pre>
+*  angular.module('demo', ['dotjem.routing']).
+*    config(['$stateProvider', function ($stateProvider) {
+*      $stateProvider
+*        .state('phones', {
+*          views: {
+*           'main': { template: 'phones.html' },
+*           'hint': { template: { html: '@phones' } }
+*         }
+*      })
+*      .state('tablets', {
+*          views: {
+*           'main': { template: 'tablets.html' },
+*           'hint': { template: { html: '@tablets' } }
+*         }
+*       })
+*  }]);
+* </pre>
+* <br/>
+* **Note:** The template is suddenly an object with an `html` property, there is a number of ways to configure templates, see {@link dotjem.routing.$template $template} for more details on that.
+* <br/>
+* ### Controllers
+* <hr/>
+* Standing alone like this, views are very static , but just like the original angular routing, we can add controllers to a view.
+*
+* <pre>
+*  angular.module('demo', ['dotjem.routing']).
+*    config(['$stateProvider', function ($stateProvider) {
+*      $stateProvider
+*        .state('phones', {
+*          views: {
+*            'main': { template: 'phones.html', controller: 'PhonesCtrl' },
+*            'hint': { template: { html: '@phones' } }
+*          }
+*        })
+*        .state('tablets', {
+*          views: {
+*            'main': { template: 'tablets.html', controller: 'TabletsCtrl' },
+*            'hint': { template: { html: '@tablets' } }
+*          }
+*        })
+*    }])
+*    .controller('PhonesCtrl', ['$scope', function ($scope) { /*...*\/ }])
+*    .controller('TabletsCtrl', ['$scope', function ($scope) { /*...*\/ }]);
+* </pre>
+* <br/>
+* ### Nested States
+* <hr/>
+* Until now we have had a flat list of states, but this doesn't really provide many enhancements over the existing routing concept, even with multiple views, all views are always reloaded. Also it could get quite complex if views dependent on each other couldn't be arranged in a hierarchy.
+* <br/>
+* The `$stateProvider` provides configuring states in a hierarchy in two ways.
+* <br/>
+* One way is using a name convention for states where `.` is used to separate state levels. So that the state `phones.list` becomes a child of `phones`, it is important however that `phones` is defined before it's children.
+*
+* <pre>
+*  angular.module('demo', ['dotjem.routing']).
+*    config(['$stateProvider', function($stateProvider) {
+*      $stateProvider
+*          .state('phones', {
+*            views: {
+*              'main': { template: 'phones.html', controller: 'PhonesCtrl' },
+*             'hint': { template: { html: '@phones' } }
+*         }
+*     })
+*     .state('phones.list', {
+*         views: {
+*             'main.content': {
+*                 template: 'phones.list.html',
+*                 controller: 'PhonesListCtrl'
+*             },
+*             'hint': { template: { html: '@phones.list' } }
+*         }
+*     })
+*     .state('phones.detail', {
+*         views: {
+*             'main.content': {
+*                 template: 'phones.detail.html',
+*                 controller: 'PhonesDetailsCtrl'
+*             },
+*             'hint': { template: { html: '@phones.list' } }
+*         }
+*     })
+* }])
+*  .controller('PhonesCtrl', ['$scope', function ($scope) { /*...*\/ }])
+*  .controller('PhonesListCtrl', ['$scope', function ($scope) { /*...*\/ }])
+*  .controller('PhonesDetailsCtrl', ['$scope', function ($scope) { /*...*\/ }]);
+* </pre>
+*
+* The above may indicate that views also has a child to parent relation in the naming, but this is merely a good naming convention, there is no constraint on how views are named.
+* <br/>
+* It is recommended that they are unique however, unless you diliberately wish to load the same content into multiple areas of a page, if multiple views use the same name within a page, they will load the same content, but they will render independendly.
 */
 var $StateProvider = [
     '$routeProvider', 
@@ -1347,17 +1458,106 @@ var $StateProvider = [
                 * @param {Object} params Current state.
                 *
                 * @description
-                * Goes to the specified state,
+                * To transition to another state, use `goto`...
+                *
+                * - state: either the full name of a state or a state object (e.g. use `lookup` to get that object).
+                * - params: (optional) a set of parameters to use for the state.
+                *
+                * If the state has an associated route, that route will be activated and the location with change it the address bar of the browser. It is also important that all parameters are defined for such route, however if the previous state defines any of those, they won't need to be redefined.
+                *
+                * e.g. say the following states are defined:
+                *
+                * <pre>
+                * $stateProvider
+                *   .state('home', { route: '/home/:homeParam' })
+                *   .state('home.child', { route: '/child/:childParam' })
+                * </pre>
+                *
+                * To activate the `home` state, a call to goto must include the `:homeParam` e.g:
+                *
+                * <pre>
+                * $state.goto('home', { homeParam: 'goHome' } );
+                * </pre>
+                *
+                * To activate the `home.child` state, a call to goto must include the `:homeParam` and the `:childParam` e.g:
+                *
+                * <pre>
+                * $state.goto('home.child', { homeParam: 'goHome', childParam: 'goChild' } );
+                * </pre>
+                *
+                * To activate the `home.child` state when currently in the `home` state, a call to goto must include the the `:childParam` and can optionally include the `:homeParam` e.g:
+                *
+                * </pre>
+                * $state.goto('home.child', { childParam: 'goChild' } );
+                * $state.goto('home.child', { homeParam: 'goHome', childParam: 'goChild' } );
+                * </pre>
+                *
+                * We can leave out the home param as that is already defined in the current set of params, that also means we can goto home from child without specifying any params, but if we wish to change it we must specify it, the following example will demonstrate a full flow:
+                *
+                * <pre>
+                * $state.goto('home', { homeParam: 1 } );
+                * // - location set to: '/home/1'
+                *
+                * $state.goto('home.child', { childParam: 1 } );
+                * // - location set to: '/home/1/child/1'
+                *
+                * $state.goto('home');
+                * // - location set to: '/home/1'
+                *
+                * $state.goto('home.child', { homeParam: 2, childParam: 2 } );
+                * // - location set to: '/home/2/child/2'
+                *
+                * $state.goto('home.child', { childParam: 4 } );
+                * // - location set to: '/home/2/child/4'
+                *
+                * $state.goto('home.child', { homeParam: 4 } );
+                * // - location set to: '/home/4/child/4'
+                * </pre>
                 */
                 /**
                 * @ngdoc method
                 * @name dotjem.routing.$state#lookup
                 * @methodOf dotjem.routing.$state
                 *
-                * @param {string} path Expression to resolve or the full name of a state.
+                * @param {string} path Path expression to the state that can either be relative to the current state or from the root (/).
                 *
                 * @description
-                * Finds a state based on the provided expression or name.
+                * To lookup a state, use `lookup`...
+                * <br/>
+                * <br/>
+                * Path is inspired by XPath and supports a subset of that syntax.
+                *
+                * - `.` : current state
+                * - `..` : parent state
+                * - `/` : path separator
+                * - `[]` : index selector, errors on overflow
+                * - `$node()` : sibling selector, can overflow
+                *
+                * <br/><br/>
+                * Using these selectors, the following are examples of paths:
+                *
+                * - `state` : Selects `state` from the current node.
+                * - `/state` : Selects `state` from the root.
+                * - `./state` : Selects `state` from the current node.
+                * - `./state/child` : Selects `child` under `state` from the current node.
+                * - `../state` : Selects `state` under the parent of the current state.
+                * - `[0]` : Selects the first child of the current state.
+                * - `[-1]` : Selects the last child of the current state.
+                * - `./state/[-1]` : Selects the last child under `state` under current state.
+                * - `$node(1)` : Selects the next sibling of the current state.
+                * - `$node(-1)` : Selects the previous sibling of the current state.
+                *
+                * <br/><br/>
+                * **Note:** When using `$node()`, it allows for overflow. this means if you are at the last child of a state and selects `$node(1)`, it will select the first child instead.
+                * <br/><br/>
+                * Finally it is also possible to select states by their full name, however as `state` would also be a valid relative selector, full name selection is only used when the name contains a `.`, this means that if you wish to select the states directly under root by name you will have to use either the syntax above or it's full name with root included:
+                *
+                * - `root.state`: Selects `state` under root.
+                * - `/state` : Selects `state` under root.
+                *
+                * <br/><br/>
+                * Errors will be thrown in cases where the path isn't valid.
+                * The root state it self can't be selected. (The root state is implicitly defined by the system, when defining `.state('home', {...});`, the state `home` isn't a root state, it is instead a child of root.
                 */
                 /**
                 * @ngdoc method
@@ -1367,7 +1567,17 @@ var $StateProvider = [
                 * @param {State|string|boolean=} state Name or State in the current hierachy or true/false
                 *
                 * @description
-                * Reloads the state and associated views.
+                * To force reload a state, use `reload`...
+                *
+                * - call `.reload()` to reload only the current leaf state.
+                * - call `.reload(true)` to reload all active states from the root state to the current leaf.
+                * - call `.reload('state.full.name')` to reload all states from `state.full.name` and down to the current leaf.
+                * <br/><br/>
+                * E.g. if the current state is `state.full.name.to.here` and `.reload()` is called then all views etc. will be reloaded for the leaf state `here`.
+                * <br/><br/>
+                * If `.reload(true)` called, views etc. will be reloaded for all the states `state`, `full`, `name`, `to`, `here`.
+                * <br/><br/>
+                * Finally if  `.reload('state.full.name')` is called, views etc. will be reloaded for the states `name`, `to`, `here`.
                 */
                 /**
                 * @ngdoc method
@@ -1378,7 +1588,9 @@ var $StateProvider = [
                 * @param {Object=} params A set of parameters to use when generating the url
                 *
                 * @description
-                * An url generated from the provided parameters.
+                * To build a url for a particular state, use `url`...
+                * <br/><br/>
+                * If the state defined either by state, or current state does not have an route associated with it, it will throw an error.
                 */
                 var urlbuilder = new StateUrlBuilder($route);
                 var forceReload = null, current = root, currentParams = {
