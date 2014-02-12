c09215fa71e4e64e05e4eed6e7c9bc85839df9f4
Jens Melgaard <admin@it-links.dk>
2014-02-12 13:59:18 +0100

    Minor changes to formatting of docs

--- src/state.ts
+++ src/state.ts
@@ -20,13 +20,13 @@
  * 
  * In it self that is not really useful, but the state it self can have views added as well as onEnter / onExit handlers.
  * <br/>
- * ### Views
+ * # Views
  * <hr/>
  * At this basic level you can also configure multiple views, just add a number of `ui - view` directives with unique names, and simply target those from the configuration.
  * <br/>
  * e.g.if we had a `main` view and a `hint` view we could do.
  * 
- * <pre>
+ * <pre dx-syntax class="brush: js">
  *  angular.module('demo', ['dotjem.routing']).
  *    config(['$stateProvider', function ($stateProvider) {
  *      $stateProvider
@@ -47,11 +47,11 @@
  * <br/>
  * **Note:** The template is suddenly an object with an `html` property, there is a number of ways to configure templates, see {@link dotjem.routing.$template $template} for more details on that.
  * <br/>
- * ### Controllers
+ * # Controllers
  * <hr/>
  * Standing alone like this, views are very static , but just like the original angular routing, we can add controllers to a view.
  * 
- * <pre>
+ * <pre dx-syntax class="brush: js">
  *  angular.module('demo', ['dotjem.routing']).
  *    config(['$stateProvider', function ($stateProvider) {
  *      $stateProvider
@@ -72,7 +72,7 @@
  *    .controller('TabletsCtrl', ['$scope', function ($scope) { ... }]);
  * </pre>
  * <br/>
- * ### Nested States
+ * # Nested States
  * <hr/>
  * Until now we have had a flat list of states, but this doesn't really provide many enhancements over the existing routing concept, even with multiple views, all views are always reloaded. Also it could get quite complex if views dependent on each other couldn't be arranged in a hierarchy.
  * <br/>
@@ -80,7 +80,7 @@
  * <br/>
  * One way is using a name convention for states where `.` is used to separate state levels. So that the state `phones.list` becomes a child of `phones`, it is important however that `phones` is defined before it's children.
  * 
- * <pre>
+ * <pre dx-syntax class="brush: js">
  *  angular.module('demo', ['dotjem.routing']).
  *    config(['$stateProvider', function($stateProvider) {
  *      $stateProvider
