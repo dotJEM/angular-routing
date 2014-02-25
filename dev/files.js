4fe131570e8b1920955028c51d0dac1ef3f05fa0
Jens Melgaard <admin@it-links.dk>
2014-02-25 20:33:04 +0100

    stored list of files in seperate scripts

---
+++ dev/files.js
@@ -0,0 +1,34 @@
+﻿if (exports) {
+    var files = {
+        'src': [
+            'src/prefix',
+            'build/src/common.js',
+            'build/src/route.js',
+            'build/src/stateTransition.js',
+            'build/src/state.js',
+            'build/src/resolve.js',
+            'build/src/template.js',
+            'build/src/view.js',
+            'build/src/scroll.js',
+            'build/src/inject.js',
+            'build/src/state/state.js',
+            'build/src/state/stateBrowser.js',
+            'build/src/state/stateComparer.js',
+            'build/src/state/stateFactory.js',
+            'build/src/state/stateRules.js',
+            'build/src/state/stateUrlBuilder.js',
+            'build/src/state/transition/commands.js',
+            'build/src/state/transition/context.js',
+            'build/src/directives/jemView.js',
+            'build/src/directives/jemAnchor.js',
+            'build/src/directives/jemLink.js',
+            'src/suffix'
+        ],
+        'legacy': [
+            'src/legacy/prefix',
+            'build/src/legacy/templateDecorator.js',
+            'src/legacy/suffix'
+        ]
+    };
+    exports.files = files;
+}
--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,8 @@
 module.exports = function (grunt) {
     "use strict";
 
+    var files = require("./dev/files").files;
+
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -43,35 +45,11 @@ module.exports = function (grunt) {
                 stripBanners: true
             },
             core: {
-                src: ['src/prefix',
-                        'build/src/common.js',
-                        'build/src/route.js',
-                        'build/src/stateTransition.js',
-                        'build/src/state.js',
-                        'build/src/resolve.js',
-                        'build/src/template.js',
-                        'build/src/view.js',
-                        'build/src/scroll.js',
-                        'build/src/inject.js',
-                        'build/src/state/state.js',
-                        'build/src/state/stateBrowser.js',
-                        'build/src/state/stateComparer.js',
-                        'build/src/state/stateFactory.js',
-                        'build/src/state/stateRules.js',
-                        'build/src/state/stateUrlBuilder.js',
-                        'build/src/state/transition/commands.js',
-                        'build/src/state/transition/context.js',
-                        'build/src/directives/jemView.js',
-                        'build/src/directives/jemAnchor.js',
-                        'build/src/directives/jemLink.js',
-                        'src/suffix'],
+                src: files.src,
                 dest: 'build/<%= pkg.name %>.js'
             },
             legacy: {
-                src: ['src/legacy/prefix',
-                      'build/src/legacy/templateDecorator.js',
-                      'src/legacy/suffix'
-                ],
+                src: files.legacy,
                 dest: 'build/<%= pkg.name %>.legacy.js'
             }
         },
